28737da81eb2ad2272d5294c6cfa3d12ef8dc80c
TUSHAR SHARMA <tusharsharma991193@gmail.com>
2025-03-25 13:20:54 +0000

    datatypes

---
+++ 01_basics/5( datatypes).js
@@ -0,0 +1,29 @@
+"use strict"; // treate all js code as a newer version of js
+
+// alert("hello world") // we are using nodejs, not browser
+
+console.log(3+3)   // js avoid the white space,2)extra space,3)tab space,4) new line space
+
+console.log("tushar")
+
+let name="tushar"            // string
+let age=21                 // number                    
+let is_logged_in=true         //boolean
+
+//number=>  2 to power 53-1
+//bigint=> 2 to power 53
+//string=> "tushar" or 'tushar'
+//boolean=> true or false
+//null=> null               //stand alone value  (intensionally empty) (does not having any datatype)
+//undefined=> undefined     // jb value define nhi hoti h to undefined hoti h   variale declare kr diya h
+//symbol=> symbol()         // unique value
+
+//obejects
+
+console.log(typeof "name")  //string
+
+console.log(typeof age)   //number
+
+console.log(typeof null)   //object
+
+console.log(typeof undefined)   //undefined
