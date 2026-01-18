9c7711644ade0bbdbb4dd947f10a1a4b8af426f5
TUSHAR SHARMA <tusharsharma991193@gmail.com>
2026-01-18 15:20:16 +0530

    Refactor variable assignments and improve comments..
    Updated variable assignments and comments for clarity.

--- 01_basics/2nd __.js
+++ 01_basics/2nd __.js
@@ -16,24 +16,26 @@ account_city="New delhi"
 
 //CHANGING THE VALUES OF VARIABLES
 
-// account_id="1397243793"          const can't be reassigned
+// account_id="1397243793"             not allowed to  change / const can't be reassigned
 account_email="tusharjangra@outlook.com"
 account_password="1234567"
 account_city="Gurugram"
 
 console.log(account_email)
 
-console.table({account_id,account_email,account_password,account_city})
+console.table([ account_id,account_email,account_password,account_city ])  // by using []   
 
 // we can declare the variables without using var,let,const but it is not a good practice//
 
 // we can declare the variable using var, let.
 
-// var is a global scope variable     -----//   don't use var as variable type
-// let is a block scope variable
+/* 
+var is a global scope variable     -----//   don't use var as variable type
+let is a block scope variable
+*/
 
 // we will use ( let, const ):- because it will remove the global scope variable problem
 
 let account_state;
 
-console.log(account_state);   //undefined
+console.log(account_state);   //undefined ***************
